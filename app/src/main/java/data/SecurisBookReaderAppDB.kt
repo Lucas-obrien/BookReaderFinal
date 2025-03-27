1ad7c9b08cee4b07a1e0f0099ed9fca46ac87324
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-03-27 16:24:49 +1000

    Add DB constructor

---
+++ app/src/main/java/data/SecurisBookReaderAppDB.kt
@@ -0,0 +1,35 @@
+package data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// USE A COMMA TO ADD ADDITIONAL DBS
+// @Database(entities = [User::class, ^^^^^^^ ], version = 1, exportSchema = false)
+@Database(entities = [User::class], version = 1, exportSchema = false)
+abstract class SecurisBookReaderAppDB : RoomDatabase() {
+        abstract fun SecurisBookReaderDao(): SecurisBookReaderDao
+
+    //a companion object is similar to Java static declarations.
+        //adding companion to the object declaration allows for adding
+        // the "static" functionality to an object
+        // used to create singleton object
+        companion object {
+            @Volatile
+            private var INSTANCE: SecurisBookReaderAppDB? = null
+
+            fun getDatabase(context: Context): SecurisBookReaderAppDB {
+                //?: takes the right-hand value if the left-hand value is null (the elvis operator)
+                return INSTANCE ?: synchronized(this) {
+                    val instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        SecurisBookReaderAppDB::class.java,
+                        "user_database"
+                    ).build()
+                    INSTANCE = instance
+                    instance
+                }
+            }
+        }
+}
