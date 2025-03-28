4be0438c857c5486408108b238ba0def0b2eab16
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-03-28 10:21:09 +1000

    Add additional table to generated database

--- app/src/main/java/data/SecurisBookReaderAppDB.kt
+++ app/src/main/java/data/SecurisBookReaderAppDB.kt
@@ -4,10 +4,11 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import model.Books_Object
 
 // USE A COMMA TO ADD ADDITIONAL DBS
 // @Database(entities = [User::class, ^^^^^^^ ], version = 1, exportSchema = false)
-@Database(entities = [User::class], version = 1, exportSchema = false)
+@Database(entities = [User::class, Books_Object::class], version = 1, exportSchema = false)
 abstract class SecurisBookReaderAppDB : RoomDatabase() {
         abstract fun SecurisBookReaderDao(): SecurisBookReaderDao
 
