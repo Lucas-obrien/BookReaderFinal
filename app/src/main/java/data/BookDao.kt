c3d9857b9ee4bc995c47482c0c8dbbc33acd5263
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-03-30 13:57:53 +1000

    Create new Dao and AppDatabase

---
+++ app/src/main/java/data/BookDao.kt
@@ -0,0 +1,9 @@
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Query
+
+@Dao
+interface BookDao {
+    @Query("SELECT * FROM books")
+    fun getAllBooks(): LiveData<List<Book>>
+}
