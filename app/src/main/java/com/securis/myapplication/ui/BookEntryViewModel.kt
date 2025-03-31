0f3f9894dc75b95d3774221e841e5c2db1fd4564
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-03-31 10:32:03 +1000

    Add Book Entry View Model, rework code from Android lesson, add bookUiState for button interaction, include validation functions

---
+++ app/src/main/java/com/securis/myapplication/ui/BookEntryViewModel.kt
@@ -0,0 +1,41 @@
+
+package com.securis.myapplication.ui
+
+import BookDetails
+import BookUiState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+
+import com.securis.myapplication.data.BooksRepository
+import toBook
+
+class BookEntryViewModel(private val booksRepository: BooksRepository) : ViewModel() {
+
+    var bookUiState by mutableStateOf(BookUiState())
+        private set
+
+    fun updateUiState(bookDetails: BookDetails) {
+        bookUiState =
+            BookUiState(bookDetails = bookDetails, isEntryValid = validateInput(bookDetails))
+    }
+
+    private fun validateInput(uiState: BookDetails = bookUiState.bookDetails): Boolean {
+        return with(uiState) {
+            title.isNotBlank() && author.isNotBlank()
+        }
+    }
+    suspend fun saveItem() {
+        if (validateInput()) {
+            booksRepository.insertBook(bookUiState.bookDetails.toBook())
+        }
+    }
+
+    fun saveBook() {
+//        TODO("Not yet implemented")
+    }
+
+
+}
