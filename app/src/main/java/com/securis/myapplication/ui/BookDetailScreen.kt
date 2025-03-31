e6391a0c4d04777219d9aa8632aad4328057be12
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-03-31 10:13:36 +1000

    Add BookDetails Body function

--- app/src/main/java/com/securis/myapplication/ui/BookDetailScreen.kt
+++ app/src/main/java/com/securis/myapplication/ui/BookDetailScreen.kt
@@ -9,9 +9,14 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -19,6 +24,8 @@ import androidx.compose.ui.text.font.FontWeight
 import com.securis.myapplication.R
 import com.securis.myapplication.data.Book
 import com.securis.myapplication.navigation.NavigationDestination
+import com.securis.myapplication.ui.BookDetailsUiState
+import com.securis.myapplication.ui.toBook
 
 // NavHost directions
 object BookDetailsDestination : NavigationDestination {
@@ -26,6 +33,42 @@ object BookDetailsDestination : NavigationDestination {
     override val titleRes = R.string.book_detail_title
     const val bookIdArg = "bookId"
     val routeWithArgs = "$route/{$bookIdArg}"
+}
+
+@Composable
+fun BookDetailsBody(
+    bookDetailsUiState: BookDetailsUiState,
+    onDelete: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium)),
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        var deleteConfirmationRequired by rememberSaveable { mutableStateOf(false) }
+
+        BookDetails(
+            book = bookDetailsUiState.bookDetails.toBook(),
+            modifier = Modifier.fillMaxWidth()
+        )
+        OutlinedButton(
+            onClick = { deleteConfirmationRequired = true },
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(stringResource(R.string.delete))
+        }
+        if (deleteConfirmationRequired) {
+            DeleteConfirmationDialog(
+                onDeleteConfirm = {
+                    deleteConfirmationRequired = false
+                    onDelete()
+                },
+                onDeleteCancel = { deleteConfirmationRequired = false },
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
 }
 
 
