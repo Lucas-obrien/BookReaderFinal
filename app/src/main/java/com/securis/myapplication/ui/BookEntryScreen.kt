32219e4d7982d1a29e484ba09f7fb7eab4f18cd8
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-03-31 10:27:24 +1000

    Add book entry screen, rework android lesson code to suit other variables, TODO() add additional fields to text input

---
+++ app/src/main/java/com/securis/myapplication/ui/BookEntryScreen.kt
@@ -0,0 +1,93 @@
+package com.securis.myapplication.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import com.securis.myapplication.R
+
+@Composable
+fun BookEntryBody(
+    bookUiState: BookUiState,
+    onBookValueChange: (BookDetails) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large)),
+        modifier = modifier.padding(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        BookInputForm(
+            bookDetails = bookUiState.bookDetails,
+            onValueChange = onBookValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        // NEED TO ADD REVIEW AND RATING FIELD, ADDITIONALLY GENRE
+        Button(
+            onClick = onSaveClick,
+            enabled = bookUiState.isEntryValid,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = stringResource(R.string.save_action))
+        }
+    }
+}
+
+@Composable
+fun BookInputForm(
+    bookDetails: BookDetails,
+    modifier: Modifier = Modifier,
+    onValueChange: (BookDetails) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_medium))
+    ) {
+        OutlinedTextField(
+            value = bookDetails.title,
+            onValueChange = { onValueChange(bookDetails.copy(title = it)) },
+            label = { Text(stringResource(R.string.book_title_req)) },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+            ),
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = bookDetails.author,
+            onValueChange = { onValueChange(bookDetails.copy(author = it)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal),
+            label = { Text(stringResource(R.string.book_author_req)) },
+            colors = OutlinedTextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                unfocusedContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+                disabledContainerColor = MaterialTheme.colorScheme.secondaryContainer,
+            ),
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled) {
+            Text(
+                text = stringResource(R.string.required_fields),
+                modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
+            )
+        }
+    }
+}
