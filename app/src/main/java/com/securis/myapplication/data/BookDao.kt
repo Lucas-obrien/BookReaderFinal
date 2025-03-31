0961be2ca851b2d05dd759d7e929ece1e815e388
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-03-31 10:17:52 +1000

    Add new functions do DAO, update database target to match other code

--- app/src/main/java/com/securis/myapplication/data/BookDao.kt
+++ app/src/main/java/com/securis/myapplication/data/BookDao.kt
@@ -1,20 +1,29 @@
-import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.Query
+package com.securis.myapplication.data
+
+
+import androidx.room.*
+
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BookDao {
 
-    @Query("SELECT * FROM books")
-    fun getAllBooks(): Flow<List<Book>> // This returns a Flow
+    @Query("SELECT * FROM book_database")
+    fun getAllBooks(): Flow<List<Book>>
 
-    @Insert
-    suspend fun insert(book: Book)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertAll(books: List<Book>)
 
-    @Insert
-    suspend fun insertAll(books: List<Book>) // Insert multiple books
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertBook(book: Book)
 
+    @Query("SELECT * FROM book_database WHERE id=:id")
+    fun getBook(id: Int): Flow<Book?>
+
+    @Delete
+    suspend fun deleteBook(book: Book)
+
+    @Update
+    suspend fun updateBook(book: Book)
 
 }
