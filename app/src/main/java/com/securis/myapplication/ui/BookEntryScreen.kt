3c07c218eba5c29d3ff230967d0afcea84b9dc9f
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-03-31 10:28:11 +1000

    Add Entry screen scaffold function, add preview function

--- app/src/main/java/com/securis/myapplication/ui/BookEntryScreen.kt
+++ app/src/main/java/com/securis/myapplication/ui/BookEntryScreen.kt
@@ -1,21 +1,76 @@
 package com.securis.myapplication.ui
 
+import BookReaderTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.securis.myapplication.R
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BookEntryScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    canNavigateBack: Boolean = true,
+    viewModel: BookEntryViewModel = viewModel(factory = AppViewModelProvider.Factory)
+
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        topBar = {
+            BookReaderTopAppBar(
+                title = stringResource(BookEntryDestination.titleRes),
+                canNavigateBack = canNavigateBack,
+                navigateUp = onNavigateUp
+            )
+        }
+    ) { innerPadding ->
+        BookEntryBody(
+            bookUiState = viewModel.bookUiState,
+            onBookValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.saveBook()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(
+                    start = innerPadding.calculateStartPadding(LocalLayoutDirection.current),
+                    end = innerPadding.calculateEndPadding(LocalLayoutDirection.current),
+                    top = innerPadding.calculateTopPadding()
+                )
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+
 
 @Composable
 fun BookEntryBody(
@@ -89,5 +144,18 @@ fun BookInputForm(
                 modifier = Modifier.padding(start = dimensionResource(id = R.dimen.padding_medium))
             )
         }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun BookEntryScreenPreview() {
+    BookReaderTheme {
+        BookEntryBody(bookUiState = BookUiState(
+            BookDetails(
+                title = "Title", author = "Robert"
+            )
+        ), onBookValueChange = {}, onSaveClick = {})
     }
 }
