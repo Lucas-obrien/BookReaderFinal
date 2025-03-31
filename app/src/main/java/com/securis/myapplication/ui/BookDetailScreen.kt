9adbc753b8973cc6e9ceae4b94ac56a2767c7633
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-03-31 10:14:48 +1000

    Add Book screen function with scaffold and Preview composable

--- app/src/main/java/com/securis/myapplication/ui/BookDetailScreen.kt
+++ app/src/main/java/com/securis/myapplication/ui/BookDetailScreen.kt
@@ -1,31 +1,47 @@
+package com.securis.myapplication.ui
+
+import BookReaderTheme
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.securis.myapplication.R
 import com.securis.myapplication.data.Book
 import com.securis.myapplication.navigation.NavigationDestination
-import com.securis.myapplication.ui.BookDetailsUiState
-import com.securis.myapplication.ui.toBook
+
 
 // NavHost directions
 object BookDetailsDestination : NavigationDestination {
@@ -34,6 +50,54 @@ object BookDetailsDestination : NavigationDestination {
     const val bookIdArg = "bookId"
     val routeWithArgs = "$route/{$bookIdArg}"
 }
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BookDetailScreen(
+    navigateToEditBook: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: BookDetailViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+    val uiState = viewModel.uiState.collectAsState()
+
+
+    Scaffold(
+        topBar = {
+            BookReaderTopAppBar(
+                title = stringResource(BookDetailsDestination.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        }, floatingActionButton = {
+            FloatingActionButton(
+                onClick = { navigateToEditBook(0) },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
+
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.edit_book_title),
+                )
+            }
+        }, modifier = modifier
+    ) { innerPadding ->
+        BookDetailsBody(
+            bookDetailsUiState = uiState.value,
+            onDelete = { },
+            modifier = Modifier
+                .padding(
+                    start = innerPadding.calculateStartPadding(LocalLayoutDirection.current),
+                    end = innerPadding.calculateEndPadding(LocalLayoutDirection.current),
+                    top = innerPadding.calculateTopPadding()
+                )
+                .verticalScroll(rememberScrollState())
+        )
+    }
+}
+
 
 @Composable
 fun BookDetailsBody(
@@ -140,4 +204,18 @@ private fun DeleteConfirmationDialog(
                 Text(stringResource(R.string.yes))
             }
         })
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BookDetailsScreenPreview() {
+    BookReaderTheme {
+        BookDetailsBody(
+            BookDetailsUiState(
+                outOfStock = true,
+                bookDetails = com.securis.myapplication.ui.BookDetails(1, "Pen", "200")
+            ),
+            onDelete = {}
+        )
+    }
 }
