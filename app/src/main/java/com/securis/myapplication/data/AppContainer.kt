2f03f41276856f4a7b8e80fa705b66e454014ec2
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-03-31 10:15:31 +1000

    Add Appcontainer for database and repository

---
+++ app/src/main/java/com/securis/myapplication/data/AppContainer.kt
@@ -0,0 +1,17 @@
+package com.securis.myapplication.data
+
+import android.content.Context
+
+class AppDataContainer(private val context: Context) {
+
+    // Lazy initialization of the AppDatabase
+    private val appDatabase by lazy {
+        AppDatabase.getDatabase(context.applicationContext)
+    }
+
+    // Initialize the repository after the database is initialized
+    val booksRepository: BooksRepository by lazy {
+        // Make sure the AppDatabase instance is initialized before accessing the repository
+        BooksRepositoryImpl(appDatabase.bookDao())
+    }
+}
