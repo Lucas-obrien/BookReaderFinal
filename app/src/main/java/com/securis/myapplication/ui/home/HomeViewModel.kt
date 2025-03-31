6c9ad85a580257cba1969088149ec11ef7187d46
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-03-31 10:39:03 +1000

    Rework view model to suit this code

--- app/src/main/java/com/securis/myapplication/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/securis/myapplication/ui/home/HomeViewModel.kt
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.securis.myapplication.ui.home
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
@@ -25,9 +9,7 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
 
-/**
- * ViewModel to retrieve all items in the Room database.
- */
+
 class HomeViewModel(booksRepository: BooksRepository): ViewModel() {
 
     val homeUiState: StateFlow<HomeUiState> =
@@ -42,7 +24,4 @@ class HomeViewModel(booksRepository: BooksRepository): ViewModel() {
     }
 }
 
-/**
- * Ui State for HomeScreen
- */
 data class HomeUiState(val itemList: List<Book> = listOf())
