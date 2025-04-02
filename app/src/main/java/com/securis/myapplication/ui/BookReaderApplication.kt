7eb73dcd2652379d5527b68936aa99af2e9be324
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-04-02 19:53:21 +1000

    Remove CreationExtras from file

--- app/src/main/java/com/securis/myapplication/ui/BookReaderApplication.kt
+++ app/src/main/java/com/securis/myapplication/ui/BookReaderApplication.kt
@@ -3,8 +3,6 @@ package com.securis.myapplication.ui
 
 
 import android.app.Application
-import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewmodel.CreationExtras
 import com.securis.myapplication.data.AppDataContainer
 
 class BookReaderApplication : Application() {
@@ -14,14 +12,7 @@ class BookReaderApplication : Application() {
         super.onCreate()
         container = AppDataContainer(this)
     }
-    fun CreationExtras.bookReaderApplication(): BookReaderApplication {
-        val application = this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY]
-        if (application is BookReaderApplication) {
-            return application
-        } else {
-            throw IllegalStateException("Application is not of type BookReaderApplication")
-        }
-    }
+
 
 }
 
