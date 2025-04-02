06e86046a57ffe90464384edef4c0fe49c46a41a
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-04-02 19:52:43 +1000

    Add view model functionality to ManageBook, add Destination object, scaffold and layout for ManageBookScreen, Add BookManageItem for individual book objects

--- app/src/main/java/com/securis/myapplication/ui/manageBookScreen.kt
+++ app/src/main/java/com/securis/myapplication/ui/manageBookScreen.kt
@@ -6,13 +6,20 @@
 package com.securis.myapplication.ui
 import android.content.Context
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -22,28 +29,95 @@ import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
-import androidx.room.Room
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.securis.myapplication.R
-import com.securis.myapplication.data.AppDatabase
 import com.securis.myapplication.data.Book
-import com.securis.myapplication.ui.home.MenuButton
+import com.securis.myapplication.navigation.NavigationDestination
 
+object BookManageDestination : NavigationDestination {
+    override val route = "book_manage"
+    override val titleRes = R.string.edit_book_title
+    const val itemIdArg = "bookId"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ManageBookApp(navController: NavHostController, context: Context) {
-    val db = Room.databaseBuilder(
-        context,
-        AppDatabase::class.java, "books_database"
-    ).build()
+fun ManageBookScreen(
+    onBookEditClick: (Int) -> Unit,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+    viewModel: ManageBooksViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+    val uiState = viewModel.manageUiState.collectAsState()
 
-    val books = db.bookDao().getAllBooks().collectAsState(initial = emptyList()).value
-
-    Column(modifier = Modifier.fillMaxWidth()) {
-        MenuButton(navController)
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = stringResource(R.string.app_name),
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                }
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+        ) {
+            if (uiState.value.bookList.isEmpty()) {
+                Text(
+                    text = "I AM HERE", // placeholder or use stringResource(R.string.no_item_description)
+                    textAlign = TextAlign.Center,
+                    style = MaterialTheme.typography.titleLarge,
+                    modifier = Modifier.padding(contentPadding),
+                )
+            } else {
+                LazyColumn(
+                    contentPadding = PaddingValues(horizontal = 16.dp, vertical = 12.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    items(uiState.value.bookList, key = { it.id }) { book ->
+                        BookManageItem(book = book, onClick = { onBookEditClick(book.id) })
+                    }
+                }
+            }
+        }
     }
 }
 
+
+@Composable
+fun BookManageItem(book: Book, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onClick() },
+        elevation = CardDefaults.cardElevation(4.dp),
+        shape = MaterialTheme.shapes.medium
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(
+                text = book.title,
+                style = MaterialTheme.typography.titleMedium
+            )
+            Text(
+                text = "by ${book.author}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
+
+
+
+// REMOVE THIS AS NO LONGER USED
 @Composable
 private fun ManageBookBody(
     bookList: List<Book>,
