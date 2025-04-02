fc8cf2749759d5728de05beb8b01fe0e18ce3392
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-04-02 19:55:46 +1000

    Add functionality for updating book, include error checking function for required fields

--- app/src/main/java/com/securis/myapplication/ui/BookEditViewModel.kt
+++ app/src/main/java/com/securis/myapplication/ui/BookEditViewModel.kt
@@ -5,16 +5,48 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
-
-
+import androidx.lifecycle.viewModelScope
+import com.securis.myapplication.data.BooksRepository
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.firstOrNull
 
 class BookEditViewModel(
     savedStateHandle: SavedStateHandle,
+    private val booksRepository: BooksRepository
 ) : ViewModel() {
-
 
     var bookUiState by mutableStateOf(BookUiState())
         private set
 
+    private val bookId: Int = checkNotNull(savedStateHandle[BookEditDestination.bookIdArg])
 
+    init {
+        viewModelScope.launch {
+            val book = booksRepository.getBookStream(bookId).firstOrNull()
+            if (book != null) {
+                bookUiState = book.toBookUiState(isEntryValid = true)
+            }
+        }
+    }
+
+    fun updateUiState(bookDetails: BookDetails) {
+        bookUiState = BookUiState(
+            bookDetails = bookDetails,
+            isEntryValid = validateInput(bookDetails)
+        )
+    }
+
+    fun updateBook() {
+        viewModelScope.launch {
+            if (bookUiState.isEntryValid) {
+                booksRepository.updateBook(bookUiState.bookDetails.toBook())
+            }
+        }
+    }
+
+    private fun validateInput(uiState: BookDetails = bookUiState.bookDetails): Boolean {
+        return with(uiState) {
+            title.isNotBlank() && author.isNotBlank()
+        }
+    }
 }
