05ddf0ff663abff3a212d52eba7af4c7a1e15a7b
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-04-02 19:57:52 +1000

    Remove redundant comments

--- app/src/main/java/com/securis/myapplication/ui/theme/Theme.kt
+++ app/src/main/java/com/securis/myapplication/ui/theme/Theme.kt
@@ -82,7 +82,6 @@ private val DarkColorScheme = darkColorScheme(
 @Composable
 fun BookReaderTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+, turned off for training purposes
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
@@ -109,18 +108,12 @@ fun BookReaderTheme(
     )
 }
 
-/**
- * Sets up edge-to-edge for the window of this [view]. The system icon colors are set to either
- * light or dark depending on whether the [darkTheme] is enabled or not.
- */
 private fun setUpEdgeToEdge(view: View, darkTheme: Boolean) {
     val window = (view.context as Activity).window
     WindowCompat.setDecorFitsSystemWindows(window, false)
     window.statusBarColor = Color.Transparent.toArgb()
     val navigationBarColor = when {
         Build.VERSION.SDK_INT >= 29 -> Color.Transparent.toArgb()
-        Build.VERSION.SDK_INT >= 26 -> Color(0xFF, 0xFF, 0xFF, 0x63).toArgb()
-        // Min sdk version for this app is 24, this block is for SDK versions 24 and 25
         else -> Color(0x00,0x00, 0x00, 0x50).toArgb()
     }
     window.navigationBarColor = navigationBarColor
