504eb698e991e3689271c5ac8db3193f71de7514
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-04-03 13:02:20 +1000

    Update Book object review to blurb

--- app/src/main/java/com/securis/myapplication/data/BooksRepositoryImpl.kt
+++ app/src/main/java/com/securis/myapplication/data/BooksRepositoryImpl.kt
@@ -9,16 +9,16 @@ class BooksRepositoryImpl(private val bookDao: BookDao) : BooksRepository {
     // Function to insert 10 books into the database
     override suspend fun addBooks() {
         val books = listOf(
-            Book(id = 1, title = "1984", author = "George Orwell", review = "Its okay", genre = "Dystopian", rating = 5),
-            Book(id = 2, title = "Brave New World", author = "Aldous Huxley", review = "Its okay", genre = "Science Fiction", rating = 5),
-            Book(id = 3, title = "Fahrenheit 451", author = "Ray Bradbury", review = "Its okay", genre = "Dystopian", rating = 5),
-            Book(id = 4, title = "The Great Gatsby", author = "F. Scott Fitzgerald", review = "Its okay", genre = "Fiction", rating = 5),
-            Book(id = 5, title = "Moby Dick", author = "Herman Melville", review = "Its okay", genre = "Fiction", rating = 5),
-            Book(id = 6, title = "War and Peace", author = "Leo Tolstoy", review = "Its okay", genre = "Historical Fiction", rating = 5),
-            Book(id = 7, title = "The Catcher in the Rye", author = "J.D. Salinger", review = "Its okay", genre = "Historical Fiction", rating = 5),
-            Book(id = 8, title = "Pride and Prejudice", author = "Jane Austen", review = "Its okay", genre = "Fiction", rating = 5),
-            Book(id = 9, title = "The Hobbit", author = "J.R.R. Tolkien", review = "Its okay", genre = "Fantasy Fiction", rating = 5),
-            Book(id = 10, title = "The Lord of the Rings", author = "J.R.R. Tolkien", review = "Its okay", genre = "Fantasy Fiction", rating = 5)
+            Book(id = 1, title = "1984", author = "George Orwell", blurb = "Its okay", genre = "Dystopian", rating = 5),
+            Book(id = 2, title = "Brave New World", author = "Aldous Huxley", blurb = "Its okay", genre = "Science Fiction", rating = 5),
+            Book(id = 3, title = "Fahrenheit 451", author = "Ray Bradbury", blurb = "Its okay", genre = "Dystopian", rating = 5),
+            Book(id = 4, title = "The Great Gatsby", author = "F. Scott Fitzgerald", blurb = "Its okay", genre = "Fiction", rating = 5),
+            Book(id = 5, title = "Moby Dick", author = "Herman Melville", blurb = "Its okay", genre = "Fiction", rating = 5),
+            Book(id = 6, title = "War and Peace", author = "Leo Tolstoy", blurb = "Its okay", genre = "Historical Fiction", rating = 5),
+            Book(id = 7, title = "The Catcher in the Rye", author = "J.D. Salinger", blurb = "Its okay", genre = "Historical Fiction", rating = 5),
+            Book(id = 8, title = "Pride and Prejudice", author = "Jane Austen", blurb = "Its okay", genre = "Fiction", rating = 5),
+            Book(id = 9, title = "The Hobbit", author = "J.R.R. Tolkien", blurb = "Its okay", genre = "Fantasy Fiction", rating = 5),
+            Book(id = 10, title = "The Lord of the Rings", author = "J.R.R. Tolkien", blurb = "Its okay", genre = "Fantasy Fiction", rating = 5)
         )
 
         withContext(Dispatchers.IO) {
