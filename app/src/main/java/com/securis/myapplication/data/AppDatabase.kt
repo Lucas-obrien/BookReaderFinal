e88ffcbcfa4ed85249707ed64d012471089d7450
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-04-03 13:00:56 +1000

    Remove commented code

--- app/src/main/java/com/securis/myapplication/data/AppDatabase.kt
+++ app/src/main/java/com/securis/myapplication/data/AppDatabase.kt
@@ -9,7 +9,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-@Database(entities = [Book::class], version = 5, exportSchema = false)
+@Database(entities = [Book::class], version = 7, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun bookDao(): BookDao
 
@@ -21,14 +21,13 @@ abstract class AppDatabase : RoomDatabase() {
             return Instance ?: synchronized(this) { // Return Database instance if not initialised, else make database
                 Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "book_database")
                     .fallbackToDestructiveMigration() // Incase of mismatch, delete and rebuild
-                    .addCallback(object : RoomDatabase.Callback() {
+                    .addCallback(object : Callback() {
                         // Only run when DB is first created
                         override fun onCreate(db: SupportSQLiteDatabase) {
                             super.onCreate(db)
                             Instance?.let { database ->
                                 CoroutineScope(Dispatchers.IO).launch {
                                     // Prepopulate DB
-//                                    database.bookDao().insertAll(SampleBooks.list)
                                     prepopulateBooksFromApi(database.bookDao())
                                 }
                             }
