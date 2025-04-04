35c62a9ba78a02157089955f07172394b1682235
Lucas-obrien <lucas.obrien@my.jcu.edu.au>
2025-04-04 14:18:51 +1000

    Adjust query for repeated requests to API

--- app/src/main/java/com/securis/myapplication/ui/network/GoogleBooksAPI.kt
+++ app/src/main/java/com/securis/myapplication/ui/network/GoogleBooksAPI.kt
@@ -1,10 +1,14 @@
 package com.securis.myapplication.ui.network
 
-import com.securis.myapplication.ui.network.*
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface GoogleBooksApi {
+    // Function that increases no. of queries to Google API, Google API caps at 40 responses per req
     @GET("volumes")
-    suspend fun searchBooks(@Query("q") query: String): BooksResponse
+    suspend fun searchBooks(
+        @Query("q") query: String,
+        @Query("maxResults") maxResults: Int = 40,
+        @Query("startIndex") startIndex: Int = 0
+    ): BooksResponse
 }
